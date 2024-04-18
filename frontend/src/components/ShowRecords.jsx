a64b3adabf39e7bfc2c88212256b1fe3b6984b28
Ragav-2002 <sripalasettyraghavendra2002@gmail.com>
2024-04-18 17:27:53 +0530

    build changes 2

--- frontend/src/components/ShowRecords.jsx
+++ frontend/src/components/ShowRecords.jsx
@@ -6,6 +6,7 @@ export default function ShowRecords(){
     const {records} = useContext(RecordContext)
     return (
         <>
+        <h2>Total Records - {records.length}</h2>
         <ul>
             {records && records.map(record=>{
                 return <ShowRecord key={record._id} record={record}/>
